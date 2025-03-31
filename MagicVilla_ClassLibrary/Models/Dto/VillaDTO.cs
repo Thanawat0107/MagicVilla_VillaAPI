10282c1f63d231d40b75acec24198e0efddbe5aa
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-03-31 13:55:41 +0700

    ModelState Validations &  3-6 Custom ModelState Validation

--- MagicVilla_ClassLibrary/Models/Dto/VillaDTO.cs
+++ MagicVilla_ClassLibrary/Models/Dto/VillaDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace MagicVilla_ClassLibrary.Models.Dto
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(30)]
         public string Name { get; set; }
     }
 }
