428f4d202c62ae5c4283c58246e0005828483350
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-07 10:47:35 +0700

    Add APIRequest Model & Project Reference

---
+++ MagicVilla_ClassLibrary/Models/APIRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using static MagicVilla_ClassLibrary.Utility.SD;
+
+namespace MagicVilla_ClassLibrary.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
