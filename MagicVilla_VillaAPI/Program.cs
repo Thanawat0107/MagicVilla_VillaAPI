75e79f6c8139ad479821cdfe02364db8a0ae5d87
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-07 10:02:20 +0700

    CRUD VillaNumber & Implement Repository

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(opt =>
 
 //SERVICE
 builder.Services.AddScoped<IVillaRepository, VillaRepository>();
+builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 
 var app = builder.Build();
 
