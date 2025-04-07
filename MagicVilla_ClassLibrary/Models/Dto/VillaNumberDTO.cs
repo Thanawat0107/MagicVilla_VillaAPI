ce1998e9ab17498dfd1c776e0153c59efd824805
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-07 15:37:00 +0700

    Get Villa Numbers

--- MagicVilla_ClassLibrary/Models/Dto/VillaNumberDTO.cs
+++ MagicVilla_ClassLibrary/Models/Dto/VillaNumberDTO.cs
@@ -14,5 +14,6 @@ namespace MagicVilla_ClassLibrary.Models.Dto
         [Required]
         public int VillaID { get; set; }
         public string SpecialDetails { get; set; }
+        public VillaDTO Villa { get; set; }
     }
 }
