a24ee12b3c2852dcc96eea36776e366c9ed0ddd8
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-07 09:17:24 +0700

    Add Villa Number Models & Migration

---
+++ MagicVilla_ClassLibrary/Models/Dto/VillaNumberDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_ClassLibrary.Models.Dto
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        [Required]
+        public int VillaID { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
