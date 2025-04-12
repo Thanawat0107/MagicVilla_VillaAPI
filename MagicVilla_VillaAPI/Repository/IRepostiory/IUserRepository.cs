d997772c91af59a54ee3188d31e44b587641f4f6
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-12 10:15:21 +0700

    Add User Repository and Implement Register

---
+++ MagicVilla_VillaAPI/Repository/IRepostiory/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_ClassLibrary.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Repository.IRepostiory
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<TokenDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
