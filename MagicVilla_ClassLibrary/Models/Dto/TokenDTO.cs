8e0911851138c10ab366d947b2a5114d0b1ac4b9
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-12 10:12:05 +0700

    Add Models for Login and Registeration & Migration

---
+++ MagicVilla_ClassLibrary/Models/Dto/TokenDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_ClassLibrary.Models.Dto
+{
+    public class TokenDTO
+    {
+        public LocalUser User { get; set; }
+        public string Token { get; set; }
+    }
+}
