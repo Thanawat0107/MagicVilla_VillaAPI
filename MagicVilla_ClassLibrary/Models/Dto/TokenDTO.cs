7c9d1c414f1f1f77b01daeb6def411cd25ba3c58
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-20 09:23:29 +0700

    Clean TokenDTO

--- MagicVilla_ClassLibrary/Models/Dto/TokenDTO.cs
+++ MagicVilla_ClassLibrary/Models/Dto/TokenDTO.cs
@@ -8,7 +8,6 @@ namespace MagicVilla_ClassLibrary.Models.Dto
 {
     public class TokenDTO
     {
-        public UserDTO User { get; set; }
-        public string Token { get; set; }
+        public string AccessToken { get; set; }
     }
 }
