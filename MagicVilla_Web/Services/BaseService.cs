de897d2ac989f753bcbca7bf26e03814fef13367
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-04-21 09:20:52 +0700

    Web : Session with Tokens

--- MagicVilla_Web/Services/BaseService.cs
+++ MagicVilla_Web/Services/BaseService.cs
@@ -52,13 +52,6 @@ namespace MagicVilla_Web.Services
                     }
                     message.RequestUri = new Uri(apiRequest.Url);
 
-                    //แนบโทเคนไปกับ header
-                    if (withBearer && _tokenProvider.GetToken() != null)
-                    {
-                        var token = _tokenProvider.GetToken();
-                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
-                    }
-
                     //ตรวจสอบการแนบไฟล์
                     if (apiRequest.ContentType == SD.ContentType.MultipartFormData)
                     {
@@ -215,7 +208,7 @@ namespace MagicVilla_Web.Services
             var content = await response.Content.ReadAsStringAsync();
             var apiResponse = JsonConvert.DeserializeObject<APIResponse>(content);
 
-            if (apiResponse?.IsSuccess != null)
+            if (apiResponse?.IsSuccess != true)
             {
                 await _httpContextAccessor.HttpContext.SignOutAsync();
                 _tokenProvider.ClearToken();
